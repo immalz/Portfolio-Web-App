4dea34f827909595319d34a89548732bb33a4c92
marco alzamora <alzamora1008@gmail.com>
2020-05-19 15:55:27 -0500

    agregando enlaces a los proyectos

--- src/config/models/proyectos.js
+++ src/config/models/proyectos.js
@@ -1,27 +1,31 @@
 const projects = [{
     "name": "Web Desing",
     "img": "https://i.imgur.com/wMr1b8h.png",
-    "description": "Paginas web a su medida"
+    "direccion": "https://github.com/immalz/Web-Desing"
   },{
     "name": "Landing Page",
     "img": "https://i.imgur.com/3Xqb5rH.png",
-    "description": "---"
+    "direccion": "https://github.com/immalz/Landing-Page"
   },{
     "name": "The Conway Game",
     "img": "https://i.imgur.com/wvdTPUe.png",
-    "description": "---"
+    "direccion": "https://github.com/immalz/Conway-s_Game"
   },{
     "name": "Text Message App",
     "img": "https://i.imgur.com/cp4B1NN.png",
-    "description": "---"
+    "direccion": "https://github.com/immalz/SMS-App-Node"
   },{
     "name": "Netflix Clon",
     "img": "https://i.imgur.com/9bDD5AN.png",
-    "description": "---"
+    "direccion": "---"
   },{
     "name": "Youtube Clon",
     "img": "https://i.imgur.com/nFWA0XR.png",
     "description": "---"
+  },{
+    "name": "Note App CRUD",
+    "img": "https://i.imgur.com/cIFywkt.png",
+    "direccion": "https://github.com/immalz/Note-App-CRUD"
   }]
 
   module.exports = projects
