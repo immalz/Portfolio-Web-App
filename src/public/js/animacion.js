9100b9eeb17f218ed869a9e98745fb5c97b12ce1
immalz <alzamora1008@gmail.com>
2020-10-15 09:24:31 -0500

    cambio responsive

--- src/public/js/animacion.js
+++ src/public/js/animacion.js
@@ -21,27 +21,3 @@ open.addEventListener("click", function() {
         changeIcon = true;
     }
 });
-
-
-let countDowDate = new Date("Dec 25, 2020 00:00:00").getTime();
-
-let x = setInterval(function() {
-    let now = new Date().getTime();
-
-    var distance = countDowDate - now;
-
-
-
-    let day = Math.floor(distance / (1000 * 60 * 90 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 90 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-
-    document.getElementById('launch').innerHTML = day + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById('launch').innerHTML = 'EXPIRED';
-    }
-}, 1000);
